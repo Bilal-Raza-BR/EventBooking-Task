8026ced01741d8d8ee723b1e6072d0f444f8483f
Muhammad Bilal Raza <bilalraza2646@gmail.com>
2025-05-27 01:06:01 +0500

    'EventBooking'

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import AdminLogin from './pages/AdminLogin';
-import AdminPanel from './pages/AdminPanel';
+import Home from './pages/Home.jsx';
+import AdminLogin from './pages/AdminLogin.jsx';
+import AdminPanel from './pages/AdminPanel.jsx';
 import './app.css';
 
 function App() {
@@ -25,6 +25,8 @@ function App() {
         />
       </Routes>
     </Router>
+   
     
   );
 }
+export default App;
