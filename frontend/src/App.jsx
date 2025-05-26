53bdd0e5f20f0f5222f7393b78dbbf1ee5310452
Muhammad Bilal Raza <bilalraza2646@gmail.com>
2025-05-27 00:35:21 +0500

    EventBooking

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,30 @@
-import { useState } from 'react'
-
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import AdminLogin from './pages/AdminLogin';
+import AdminPanel from './pages/AdminPanel';
+import './app.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-
-    </>
-  )
+    
+  
+    
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/adminLogin" element={<AdminLogin />} />
+        <Route 
+          path="/adminPanel" 
+          element={
+            localStorage.getItem("isAdmin") ? (
+              <AdminPanel />
+            ) : (
+              <Navigate to="/adminLogin" replace />
+            )
+          } 
+        />
+      </Routes>
+    </Router>
+    
+  );
 }
-
-export default App
